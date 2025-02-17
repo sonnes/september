2b39941a84c6b27dbe6f2ec4c332b9499c160058
Ravi Atluri <raviatluri@gmail.com>
2025-02-17 18:43:12 +0530

    Update ESLint Configuration to Customize Rules
    - Modify ESLint config to use FlatCompat with extended configuration
    - Disable specific ESLint and TypeScript rules to reduce strict linting
    - Improve configuration readability and flexibility

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'prefer-const': 'off',
+      'react/display-name': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-img-element': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  }),
 ];
-
 export default eslintConfig;
