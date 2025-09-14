cc2dc0ba32e2eb6f5658b4a2be7e328356dc67bf
Ravi Atluri <raviatluri@gmail.com>
2025-09-14 15:41:50 +0530

    Add .env.example file and update Supabase keys in client and server files

--- supabase/client.ts
+++ supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 const supabase = createBrowserClient(supabaseUrl, supabaseKey);
 
--- supabase/server.ts
+++ supabase/server.ts
@@ -8,7 +8,7 @@ export async function createClient() {
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
@@ -36,7 +36,7 @@ export async function updateSession(request: NextRequest) {
   });
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
